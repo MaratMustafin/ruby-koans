ae24500215a5f40ec0d79c94f88f3bf53f3a52cd
MaratMustafin <sampplayer2@gmail.com>
2020-05-26 01:07:33 +0600

    completed about_dice_project.rb

--- koans/about_dice_project.rb
+++ koans/about_dice_project.rb
@@ -5,6 +5,14 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 # class DiceSet
 #   code ...
 # end
+class DiceSet
+  def roll(a)
+    @a = a
+  end
+  def values
+    (1..@a).to_a
+  end
+end
 
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
@@ -40,7 +48,7 @@ class AboutDiceProject < Neo::Koan
     dice.roll(5)
     second_time = dice.values
 
-    assert_not_equal first_time, second_time,
+    assert_not_equal first_time.object_id, second_time.object_id,
       "Two rolls should not be equal"
 
     # THINK ABOUT IT:
